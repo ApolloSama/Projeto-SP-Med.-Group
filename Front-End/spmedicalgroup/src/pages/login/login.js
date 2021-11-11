08481437e5227f59c926b8bf75ae27e58fa86943
unknown <48967937814>
2021-11-11 17:21:18 -0300

    Front-End part 2.1

--- Front-End/spmedicalgroup/src/pages/login/login.js
+++ Front-End/spmedicalgroup/src/pages/login/login.js
@@ -1,36 +1,49 @@
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import axios from 'axios';
 import { parseJwt, usuarioAutenticado } from '../../services/auth';
 import { Link } from 'react-router-dom';
 
-export default class Login extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        email: '',
-        senha: '',
-        msgErro: '',
-        isLoading: false,
-      }
+export default function Login() {
+  const [email, setEmail] = useState( '' )
 
-    }
-      
+  const [senha, setSenha] = useState( '' )
+
+  const [errorMsg, setErrorMsg] = useState( '' )
+
+  const [isLoading, setIsLoading] = useState( false )
 }
+
 
 fazerLogin = (event) => {
 
   event.preventDefault()
-
-  this.setstate({msgErro: '', isLoading: true})
+  
+  setIsLoading(true)
+  setErrorMsg( '' )
 
   axios.post('http://localhost:5000/api/Login', {
-    email: this.state.email,
-    senha: this.state.senha
+    
+    email: email,
+    senha: senha
+
   }).then ( (resposta) => {
+
     if (resposta.status === 200) {
-      localStorage.setItem
+
+      localStorage.setItem('login-usuario', resposta.data.token)
+
+      setIsLoading( false )
+
     }
+
+  }).catch(() => {
+    setErrorMsg('Email ou senha incorretos')
+    setIsLoading(false)
   })
 
-  
+}
+
+atualizaState = (campo) => {
+
+  setSenha({ [campo.target.name] : [campo.target.value]} )
 }
